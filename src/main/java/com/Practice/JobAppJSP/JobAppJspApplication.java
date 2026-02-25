ffd28c536071f63ff7c9c8ee4de776bd659beb86
Flitworkb <flitbfamily@gmail.com>
2026-02-25 18:30:47 +0530

    inital commit of an job app using jsp as an view technologies

---
+++ src/main/java/com/Practice/JobAppJSP/JobAppJspApplication.java
@@ -0,0 +1,13 @@
+package com.Practice.JobAppJSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practice/JobAppJSP/JobAppJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practice.JobAppJSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
